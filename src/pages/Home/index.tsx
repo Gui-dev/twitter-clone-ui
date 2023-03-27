93fd0b63eb84e550522744cb1ec50a157ee45bd9
Gui-dev <adrianon10@gmail.com>
2023-03-27 04:03:41 -0300

    feat: add helper function createNewTweet

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,20 +1,46 @@
+import { FormEvent, useState } from 'react'
 import { Header } from '../../components/Header'
 import { Separator } from '../../components/Separator'
-import { Tweet } from '../../components/Tweet'
+import { ITweet, Tweet } from '../../components/Tweet'
 
 import fakeTweets from './../../tweets.json'
 
 import './style.css'
 
 export const Home = () => {
+  const [tweets, setTweets] = useState<ITweet[]>(fakeTweets)
+  const [newTweet, setNewTweet] = useState('')
+
+  const createNewTweet = (event: FormEvent) => {
+    event.preventDefault()
+    const result: ITweet = {
+      id: String(Math.random() + new Date().getTime()),
+      name: 'Gui Silva',
+      user: '@Dryix',
+      tweet: newTweet,
+      image_url: 'https://github.com/Gui-dev.png',
+      comments: 0,
+      likes: 0,
+      retweets: 0
+    }
+    setTweets([...tweets, result])
+    setNewTweet('')
+  }
+
   return (
     <div className="timeline">
       <Header title="Home" />
 
-      <form className="new-tweet-form">
+      <form onSubmit={createNewTweet} className="new-tweet-form">
         <label htmlFor="tweet">
           <img src="https://github.com/Gui-dev.png" alt="Gui Silva" />
-          <textarea id="tweet" placeholder="What's happening?"></textarea>
+          <textarea
+            id="tweet"
+            placeholder="What's happening?"
+            value={newTweet}
+            onChange={event => setNewTweet(event.target.value)}
+          >
+          </textarea>
         </label>
         <button type="submit">
           Tweet
@@ -23,7 +49,7 @@ export const Home = () => {
 
       <Separator />
 
-      {fakeTweets.map(tweet => {
+      {tweets.map(tweet => {
         return (
           <Tweet key={tweet.id} {...tweet} />
         )
